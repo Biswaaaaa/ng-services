63a09af32bf5569a42655acea59ad14a7014713c
Biswaaaaa <ibbarman@gmail.com>
2020-01-19 12:20:13 +0530

    movie modules

--- src/app/classes/data-characters.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DataCharacters } from './data-characters';
-
-describe('DataCharacters', () => {
-  it('should create an instance', () => {
-    expect(new DataCharacters()).toBeTruthy();
-  });
-});
