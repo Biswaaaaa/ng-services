6cc7a01cd030ec1b0e490ca0cae5121d247d613a
Biswaaaaa <ibbarman@gmail.com>
2020-01-19 12:57:33 +0530

    movies module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharactersListComponent } from './components/characters-list/characters-list.component';
-import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { MoviesListComponent } from './modules/movies/components/movies-list/movies-list.component';
 import { RouterModule } from '@angular/router';
-import { MovieComponent } from './components/movie/movie.component';
+import { MovieComponent } from './modules/movies/components/movie/movie.component';
 import { CharacterComponent } from './components/character/character.component';
 
 @NgModule({
--- src/app/modules/movies/components/movies-list/movies-list.component.ts
+++ src/app/modules/movies/components/movies-list/movies-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../../classes/movie';
-import { MoviesService } from '../../../../services/movies.service';
+import { MoviesService } from '../../services/movies.service';
 
 
 @Component({
